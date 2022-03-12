aa6750df55d2da41737451c3d72fc8ddbdc6ceef
Jmc <19344972121@qq.com>
2022-03-12 14:34:17 +0800

    Change config.

--- src/main/java/com/lingyuango/seckillmanagement/ManagementMain.java
+++ src/main/java/com/lingyuango/seckillmanagement/ManagementMain.java
@@ -4,17 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import xyz.erupt.core.annotation.EruptScan;
 
-import java.awt.*;
-import java.net.URI;
-
 /**
  * A22秒杀后台管理系统启动类 <br>
  * VM Options: --add-opens java.base/sun.reflect.annotation=ALL-UNNAMED
  */
 @SpringBootApplication
 @EruptScan
-public class ManagementApplication {
+public class ManagementMain {
 	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
+		SpringApplication.run(ManagementMain.class, args);
 	}
 }
